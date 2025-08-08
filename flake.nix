fa436c4880cbb064c1e48c8e898fbe8845b25fc3
Jeff Hykin <jeff.hykin@gmail.com>
2025-08-08 17:29:48 -0500

    testing idea

--- flake.nix
+++ flake.nix
@@ -58,8 +58,8 @@
                                     exit
                                 elif [ -n "$XOME_INFER_REAL_PATH_DEBUG" ]
                                 then
-                                    what_is_added_to_path_full="$(XOME_INFER_REAL_PATH=true USER=$XOME_REAL_USER PATH="/usr/local/bin:/usr/bin:/bin" ${pkgs.nix}/bin/nix develop 2>/dev/null)"
-                                    what_is_added_to_path="$(XOME_INFER_REAL_PATH=true USER=$XOME_REAL_USER PATH="/usr/local/bin:/usr/bin:/bin" ${pkgs.nix}/bin/nix develop 2>/dev/null | ${pkgs.coreutils}/bin/tail -n 1)"
+                                    what_is_added_to_path_full="$(XOME_INFER_REAL_PATH_DEBUG="" XOME_INFER_REAL_PATH=true USER=$XOME_REAL_USER PATH="/usr/local/bin:/usr/bin:/bin" ${pkgs.nix}/bin/nix develop 2>/dev/null)"
+                                    what_is_added_to_path="$(XOME_INFER_REAL_PATH_DEBUG="" XOME_INFER_REAL_PATH=true USER=$XOME_REAL_USER PATH="/usr/local/bin:/usr/bin:/bin" ${pkgs.nix}/bin/nix develop 2>/dev/null | ${pkgs.coreutils}/bin/tail -n 1)"
                                     length="''${#what_is_added_to_path}"
                                     path_minus_nix_junk="''${PATH:$length}"
                                     echo 'what_is_added_to_path_full: '"$what_is_added_to_path_full"
